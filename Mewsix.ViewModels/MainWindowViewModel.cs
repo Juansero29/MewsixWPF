40a6e02337ca509def164f25e6d5de224516ccab
Juansero29 <juansero29@gmail.com>
2017-05-20 15:52:39 +0200

    - Added sketch

--- Mewsix.ViewModels/MainWindowViewModel.cs
+++ Mewsix.ViewModels/MainWindowViewModel.cs
@@ -21,7 +21,9 @@ namespace Mewsix.ViewModels
             Songs = new ObservableCollection<Song> {
                 new Song("Creep", "Radiohead", "https://images.genius.com/dc6a81658957cf95dc7a5834b6321b7a.300x300x1.jpg"),
                 new Song("Young Stuff", "Snarky Puppy", "https://s3.amazonaws.com/bit-photos/large/6303622.jpeg"),
-                new Song("Like A Stone", "Audioslave", "https://images-na.ssl-images-amazon.com/images/I/81SPG6dHDXL._SL1500_.jpg") };
+                new Song("Like A Stone", "Audioslave", "https://images-na.ssl-images-amazon.com/images/I/81SPG6dHDXL._SL1500_.jpg"),
+                new Song("Like A Stone", "Audioslave", "https://images-na.ssl-images-amazon.com/images/I/81SPG6dHDXL._SL1500_.jpg"),
+                new Song("Like A Stone", "Audioslave", "https://images-na.ssl-images-amazon.com/images/I/81SPG6dHDXL._SL1500_.jpg")};
         }
 
         public void OnRaisePropertyChanged([CallerMemberName] string name = "")
