df4196301bc1a50da7af968cea64d4b9dddf3587
Juansero29 <juansero29@gmail.com>
2017-05-20 15:04:44 +0200

    Add project files.

---
+++ Mewsix.Models/Song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mewsix.Models
+{
+    public class Song
+    {
+        public string Name { get; private set; }
+        public string Artist { get; private set; }
+        public Uri AlbumUri { get; private set; }
+
+        public string Lyrics { get; private set; }
+
+        public Song(string name, string artist, string uri)
+        {
+            Name = name;
+            Artist = artist;
+            AlbumUri = new Uri(uri);
+        }
+    }
+}
---
+++ Mewsix.ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Mewsix.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mewsix.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Song> Songs { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Songs = new ObservableCollection<Song> {
+                new Song("Creep", "Radiohead", "https://images.genius.com/dc6a81658957cf95dc7a5834b6321b7a.300x300x1.jpg"),
+                new Song("Young Stuff", "Snarky Puppy", "https://s3.amazonaws.com/bit-photos/large/6303622.jpeg"),
+                new Song("Like A Stone", "Audioslave", "https://images-na.ssl-images-amazon.com/images/I/81SPG6dHDXL._SL1500_.jpg") };
+        }
+
+        public void OnRaisePropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
