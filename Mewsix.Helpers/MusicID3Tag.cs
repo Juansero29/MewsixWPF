d7e8fcc5ae71a5f1f1423f88ca8f9fb1efdbbadf
Gerben De Vries <gerbencdg@gmail.com>
2017-05-23 11:54:43 +0200

    - created MusicID3Tag.cs in Helpers - cleaned up MainWindowViewModel

---
+++ Mewsix.Helpers/MusicID3Tag.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Mewsix.Helpers
+{
+    public class MusicID3Tag
+    {
+        private byte[] _tagID = new byte[3];      //  3
+        private byte[] _title = new byte[30];     //  30
+        private byte[] _artist = new byte[30];    //  30 
+        private byte[] _album = new byte[30];     //  30 
+        private byte[] _year = new byte[4];       //  4 
+        private byte[] _comment = new byte[30];   //  30 
+        private byte[] _genre = new byte[1];      //  1
+
+        public string Title { get; private set; }
+        public string Artist { get; private set; }
+        public string Album { get; private set; }
+        public string Year { get; private set; }
+        public string Comment { get; private set; }
+        public string Genre { get; private set; }
+
+        public MusicID3Tag(string trackPath)
+        {
+            // https://stackoverflow.com/questions/5782642/wpf-c-get-informations-from-audio-file
+
+            using (FileStream fs = File.OpenRead(trackPath))
+            {
+                if (fs.Length >= 128)
+                {
+                    fs.Seek(-128, SeekOrigin.End);
+                    fs.Read(_tagID, 0, _tagID.Length);
+                    fs.Read(_title, 0, _title.Length);
+                    fs.Read(_artist, 0, _artist.Length);
+                    fs.Read(_album, 0, _album.Length);
+                    fs.Read(_year, 0, _year.Length);
+                    fs.Read(_comment, 0, _comment.Length);
+                    fs.Read(_genre, 0, _genre.Length);
+                    string theTAGID = Encoding.Default.GetString(_tagID);
+
+                    if (theTAGID.Equals("TAG"))
+                    {
+                        Title = Encoding.Default.GetString(_title);
+                        Artist = Encoding.Default.GetString(_artist);
+                        Album = Encoding.Default.GetString(_album);
+                        Year = Encoding.Default.GetString(_year);
+                        Comment = Encoding.Default.GetString(_comment);
+                        Genre = Encoding.Default.GetString(_genre);
+
+                        Title = Title.Replace("\0", "");
+                        Artist = Artist.Replace("\0", "");
+                    } else
+                    {
+                        Uri trackUri = new Uri(trackPath);
+                        MessageBox.Show("Incorrect tag for track : "+ trackUri.Segments[trackUri.Segments.Length-1]);
+                    }
+                }
+            }
+        }
+
+    }
+}
--- Mewsix.ViewModels/MainWindowViewModel.cs
+++ Mewsix.ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Mewsix.Models;
+﻿using Mewsix.Helpers;
+using Mewsix.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -49,48 +50,8 @@ namespace Mewsix.ViewModels
 
         private void TrackUris_OnAdd(object sender, TrackPathEventArgs<String> e)
         {
-            // https://stackoverflow.com/questions/5782642/wpf-c-get-informations-from-audio-file
-
-            using (FileStream fs = File.OpenRead(e.TrackPath))
-            {
-                if (fs.Length >= 128)
-                {
-                    MusicID3Tag tag = new MusicID3Tag();
-                    fs.Seek(-128, SeekOrigin.End);
-                    fs.Read(tag.TAGID, 0, tag.TAGID.Length);
-                    fs.Read(tag.Title, 0, tag.Title.Length);
-                    fs.Read(tag.Artist, 0, tag.Artist.Length);
-                    fs.Read(tag.Album, 0, tag.Album.Length);
-                    fs.Read(tag.Year, 0, tag.Year.Length);
-                    fs.Read(tag.Comment, 0, tag.Comment.Length);
-                    fs.Read(tag.Genre, 0, tag.Genre.Length);
-                    string theTAGID = Encoding.Default.GetString(tag.TAGID);
-
-                    if (theTAGID.Equals("TAG"))
-                    {
-                        string Title = Encoding.Default.GetString(tag.Title);
-                        string Artist = Encoding.Default.GetString(tag.Artist);
-                        string Album = Encoding.Default.GetString(tag.Album);
-                        string Year = Encoding.Default.GetString(tag.Year);
-                        string Comment = Encoding.Default.GetString(tag.Comment);
-                        string Genre = Encoding.Default.GetString(tag.Genre);
-
-                        Console.WriteLine(Title);
-                        Console.WriteLine(Artist);
-                        Console.WriteLine(Album);
-                        Console.WriteLine(Year);
-                        Console.WriteLine(Comment);
-                        Console.WriteLine(Genre);
-                        Console.WriteLine();
-
-                        Title = Title.Replace("\0", "");
-                        Artist = Artist.Replace("\0", "");
-
-                        Tracks.Add(new Track(Title, Artist, "https://images-na.ssl-images-amazon.com/images/I/81SPG6dHDXL._SL1500_.jpg"));
-
-                    }
-                }
-            }
+            MusicID3Tag tag = new MusicID3Tag(e.TrackPath);
+            Tracks.Add(new Track(tag.Title, tag.Artist, "https://images-na.ssl-images-amazon.com/images/I/81SPG6dHDXL._SL1500_.jpg"));                                
 
         }
 
@@ -105,17 +66,4 @@ namespace Mewsix.ViewModels
     }
 
 
-    class MusicID3Tag
-
-    {
-
-        public byte[] TAGID = new byte[3];      //  3
-        public byte[] Title = new byte[30];     //  30
-        public byte[] Artist = new byte[30];    //  30 
-        public byte[] Album = new byte[30];     //  30 
-        public byte[] Year = new byte[4];       //  4 
-        public byte[] Comment = new byte[30];   //  30 
-        public byte[] Genre = new byte[1];      //  1
-
-    }
 }
