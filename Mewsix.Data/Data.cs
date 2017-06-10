c07fb3fb3c11e04ab493964ec470541e7c0913fd
Juansero29 <juansero29@gmail.com>
2017-06-10 13:27:11 +0200

    - Removed useless dependecies on files.

--- Mewsix.Data/Data.cs
+++ Mewsix.Data/Data.cs
@@ -1,10 +1,6 @@
 ﻿using Mewsix.Models;
-using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.IO;
 
